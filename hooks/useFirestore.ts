4bbceb23c5e4e752af255e96a83606a5d2ad4439
Y.Yamamoto <tuki0918@gmail.com>
2024-10-26 16:19:54 +0900

    Add useFirestoreDoc hook for Firestore document retrieval

---
+++ hooks/useFirestore.ts
@@ -0,0 +1,31 @@
+import type {
+	DBDocument,
+	SmashCounterDocumentData,
+} from "@/types/firebase/firestore";
+import { smashCounterAtom } from "@/utils/atoms";
+import { db } from "@/utils/firebase";
+import { doc, onSnapshot } from "firebase/firestore";
+import { useSetAtom } from "jotai";
+import { useEffect } from "react";
+
+// TODO: support for some collection
+export const useFirestoreDoc = (docId: string) => {
+	// TODO: collectionId
+	const collectionId = "smash-view-counters";
+	const setData = useSetAtom(smashCounterAtom);
+
+	useEffect(() => {
+		// TODO: FirestoreDataConverter
+		const unsubscribe = onSnapshot(doc(db, collectionId, docId), (doc) => {
+			const data = !doc.exists()
+				? undefined
+				: {
+						...(doc.data() as DBDocument<SmashCounterDocumentData>),
+						id: doc.id,
+					};
+			setData(data);
+		});
+
+		return () => unsubscribe();
+	}, [docId, setData]);
+};
