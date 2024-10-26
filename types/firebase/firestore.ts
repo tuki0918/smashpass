9ae6abb4eadff1b46b9f5d863f856944b3bbfe33
Y.Yamamoto <tuki0918@gmail.com>
2024-10-27 04:52:40 +0900

    Add SmashCounterDocumentData type for Firestore

--- types/firebase/firestore.ts
+++ types/firebase/firestore.ts
@@ -3,6 +3,10 @@ import type { DocumentData } from "firebase/firestore";
 export type DBDocument<T extends DocumentData> = T & { id: string };
 export type DBCollection<T extends DocumentData> = DBDocument<T>[];
 
+export type SmashCounterStatus = "published" | "draft";
 export type SmashCounterDocumentData = {
+	title: string;
 	count: number;
+	status: SmashCounterStatus;
+	created_by_id: string;
 };
