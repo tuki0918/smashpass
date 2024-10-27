96fc31b28517e2034b56fad5324fefb21c3f5ec5
Y.Yamamoto <tuki0918@gmail.com>
2024-10-28 07:46:33 +0900

    Refactor page component to generate metadata for smash view counters

--- app/smash/[smashId]/page.tsx
+++ app/smash/[smashId]/page.tsx
@@ -1,6 +1,11 @@
 import { RealTimeSmashCounter } from "@/components/SmashCounter";
+import type {
+	DBDocument,
+	SmashCounterDocumentData,
+} from "@/types/firebase/firestore";
 import { db } from "@/utils/firebase";
 import { doc, getDoc, increment, setDoc } from "firebase/firestore";
+import type { Metadata } from "next";
 
 type Props = {
 	params: { smashId: string };
@@ -14,6 +19,30 @@ export default async function Page({ params }: Props) {
 			<RealTimeSmashCounter docId={smashId} />
 		</div>
 	);
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+	const { smashId } = params;
+	const docId = smashId;
+
+	// TODO: collectionId
+	const collectionId = "smash-view-counters";
+	const docRef = doc(db, collectionId, docId);
+	const docSnap = await getDoc(docRef);
+
+	if (docSnap.exists()) {
+		const data = docSnap.data() as DBDocument<SmashCounterDocumentData>;
+		// TODO: metadata
+		return {
+			title: data.title || "Default Title",
+			description: data.description || "",
+		};
+	}
+
+	return {
+		title: "Not Found",
+		description: "",
+	};
 }
 
 // TODO: Must be restricted by Firestore security rules
