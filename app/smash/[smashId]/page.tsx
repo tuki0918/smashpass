02b8db174277272d5da8ead3109a9daf9e9b2be8
Y.Yamamoto <tuki0918@gmail.com>
2024-10-28 00:40:02 +0900

    Refactor SmashCard component to add link to individual smash details

--- app/smash/[smashId]/page.tsx
+++ app/smash/[smashId]/page.tsx
@@ -1,14 +1,39 @@
 import { RealTimeSmashCounter } from "@/components/SmashCounter";
+import { db } from "@/utils/firebase";
+import { doc, getDoc, increment, setDoc } from "firebase/firestore";
 
 type Props = {
 	params: { smashId: string };
 };
 
-export default function Page({ params }: Props) {
+export default async function Page({ params }: Props) {
 	const { smashId } = params;
+	await incrementCount(smashId);
 	return (
 		<div className="min-h-screen flex items-center justify-center">
 			<RealTimeSmashCounter docId={smashId} />
 		</div>
 	);
 }
+
+// TODO: Must be restricted by Firestore security rules
+const incrementCount = async (docId: string) => {
+	try {
+		// TODO: collectionId
+		const collectionId = "smash-view-counters";
+		const docRef = doc(db, collectionId, docId);
+		const docSnap = await getDoc(docRef);
+
+		if (docSnap.exists()) {
+			await setDoc(
+				docRef,
+				{
+					count: increment(1),
+				},
+				{ merge: true },
+			);
+		}
+	} catch (err) {
+		console.error(err);
+	}
+};
