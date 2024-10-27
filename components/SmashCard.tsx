a2614ab14866d915be97ed57bc6415de18f5c8e3
Y.Yamamoto <tuki0918@gmail.com>
2024-10-28 06:23:24 +0900

    Refactor RealTimeSmashCardList component to handle case when no data is found

--- components/SmashCard.tsx
+++ components/SmashCard.tsx
@@ -126,6 +126,15 @@ export const RealTimeSmashCard: FC<{ docId: string }> = ({ docId }) => {
 export const RealTimeSmashCardList: FC<{
 	data: DBDocument<SmashCounterDocumentData>[];
 }> = ({ data }) => {
+	// TODO: UI for no data found
+	if (data.length === 0) {
+		return (
+			<div className="text-center">
+				<p className="text-gray-600 dark:text-gray-400">No data found.</p>
+			</div>
+		);
+	}
+
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 			{data.map((doc) => (
