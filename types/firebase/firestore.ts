c0be77c73dd8c1ce5b1d71a4cfdeb6366259eba9
Y.Yamamoto <tuki0918@gmail.com>
2024-10-28 06:07:05 +0900

    Refactor DBDocument type to include date and id properties

--- types/firebase/firestore.ts
+++ types/firebase/firestore.ts
@@ -1,6 +1,18 @@
-import type { DocumentData } from "firebase/firestore";
+import type { DocumentData, Timestamp } from "firebase/firestore";
 
-export type DBDocument<T extends DocumentData> = T & { id: string };
+export type DBDocumentId = {
+	id: string;
+};
+export type DBDocumentDate = {
+	created_at: Timestamp;
+	updated_at: Timestamp;
+	published_at?: Timestamp;
+	revised_at?: Timestamp;
+};
+
+export type DBDocument<T extends DocumentData> = T &
+	DBDocumentDate &
+	DBDocumentId;
 export type DBCollection<T extends DocumentData> = DBDocument<T>[];
 
 export type SmashCounterStatus = "published" | "draft";
