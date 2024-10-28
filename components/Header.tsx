f39bbb91d384529630a8768788c8bde32857c381
Y.Yamamoto <tuki0918@gmail.com>
2024-10-28 20:21:46 +0900

    Refactor Header component to use useAuth hook for authentication and update user information

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,14 +11,12 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/hooks/useAuth";
 import Link from "next/link";
 import type { FC } from "react";
-import { useState } from "react";
 
 const Header: FC = () => {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const handleLogin = () => setIsLoggedIn(true);
-	const handleLogout = () => setIsLoggedIn(false);
+	const { user, login, logout } = useAuth();
 
 	return (
 		<header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center items-center">
@@ -30,8 +28,8 @@ const Header: FC = () => {
 				</div>
 
 				<div className="flex items-center space-x-4">
-					{!isLoggedIn ? (
-						<Button onClick={handleLogin}>Login</Button>
+					{user === null ? (
+						<Button onClick={login}>Login</Button>
 					) : (
 						<DropdownMenu>
 							<DropdownMenuTrigger asChild>
@@ -40,7 +38,11 @@ const Header: FC = () => {
 									className="relative h-8 w-8 rounded-full"
 								>
 									<Avatar className="h-8 w-8">
-										<AvatarImage src="https://picsum.photos/100" alt="User" />
+										<AvatarImage
+											src={user.photoURL || ""}
+											alt="User Avatar Image"
+										/>
+										{/* TODO: dummy fallback avatar */}
 										<AvatarFallback>00</AvatarFallback>
 									</Avatar>
 								</Button>
@@ -48,16 +50,16 @@ const Header: FC = () => {
 							<DropdownMenuContent className="w-56" align="end" forceMount>
 								<DropdownMenuLabel className="font-normal">
 									<div className="flex flex-col space-y-1">
-										<p className="text-sm font-medium leading-none">username</p>
+										<p className="text-sm font-medium leading-none">
+											{user.displayName}
+										</p>
 										<p className="text-xs leading-none text-muted-foreground">
-											user@example.com
+											{user.email}
 										</p>
 									</div>
 								</DropdownMenuLabel>
 								<DropdownMenuSeparator />
-								<DropdownMenuItem onClick={handleLogout}>
-									Sign out
-								</DropdownMenuItem>
+								<DropdownMenuItem onClick={logout}>Sign out</DropdownMenuItem>
 							</DropdownMenuContent>
 						</DropdownMenu>
 					)}
