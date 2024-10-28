7c42a2270893698a4c57a02cc82a0cf22e519752
Y.Yamamoto <tuki0918@gmail.com>
2024-10-28 18:19:05 +0900

    Refactor global.d.ts to update process.env typings for Firebase configuration

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,17 +1,17 @@
-interface PublicRuntimeConfig {
-	SITE_NAME: string;
-	SITE_DESCRIPTION: string;
-	DB_FIRESTORE_SMASH_COLLECTION_NAME: string;
-}
-
-// biome-ignore lint/suspicious/noEmptyInterface: empty
-interface ServerRuntimeConfig {}
-
-declare module "next/config" {
-	declare const getConfig: () => {
-		publicRuntimeConfig: PublicRuntimeConfig;
-		serverRuntimeConfig: ServerRuntimeConfig;
-	};
-
-	export default getConfig;
+declare module "process" {
+	global {
+		namespace NodeJS {
+			interface ProcessEnv {
+				[key: string]: string | undefined;
+				/** firebase for client-side and server-side */
+				readonly NEXT_PUBLIC_FIREBASE_API_KEY?: string;
+				readonly NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN?: string;
+				readonly NEXT_PUBLIC_FIREBASE_PROJECT_ID?: string;
+				readonly NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET?: string;
+				readonly NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID?: string;
+				readonly NEXT_PUBLIC_FIREBASE_APP_ID?: string;
+				readonly NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID?: string;
+			}
+		}
+	}
 }
