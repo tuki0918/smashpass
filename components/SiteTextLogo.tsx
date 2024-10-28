4fd92db72eecd3bd0d84186f289637ae74baed7c
Y.Yamamoto <tuki0918@gmail.com>
2024-10-28 14:43:28 +0900

    Refactor SiteTextLogo component font size and position

--- components/SiteTextLogo.tsx
+++ components/SiteTextLogo.tsx
@@ -3,9 +3,9 @@ import type { FC } from "react";
 const SiteTextLogo: FC = () => {
 	return (
 		<div className="flex items-center justify-center h-screen">
-			<div className="relative text-xl font-bold">
+			<div className="relative text-2xl font-bold">
 				123
-				<span className="absolute -top-2 text-sm">++</span>
+				<span className="absolute -top-1.5 text-sm">++</span>
 			</div>
 		</div>
 	);
