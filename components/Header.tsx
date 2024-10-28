2c5a5266625c609ae5b24289614f9d81e00d15fd
Y.Yamamoto <tuki0918@gmail.com>
2024-10-28 18:24:02 +0900

    Refactor Header component to update container height and remove unnecessary CSS classes

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,7 +22,7 @@ const Header: FC = () => {
 
 	return (
 		<header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center items-center">
-			<div className="container flex items-center justify-between mx-2 my-2">
+			<div className="container h-12 flex items-center justify-between mx-2">
 				<div className="flex items-center space-x-2">
 					<Link href={"/"}>
 						<SiteTextLogo />
