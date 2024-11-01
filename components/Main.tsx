d754f66682ebefa4c4518b09b7af2c16a060ca15
Y.Yamamoto <tuki0918@gmail.com>
2024-11-01 22:54:42 +0900

    Refactor LoginButton component to use Google sign-in icon and add links to Terms of Service and Privacy Policy

--- components/Main.tsx
+++ components/Main.tsx
@@ -3,24 +3,44 @@
 import LoginButton from "@/components/LoginButton";
 import SiteTextLogo from "@/components/SiteTextLogo";
 import { useAuth } from "@/hooks/useAuth";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import type { FC } from "react";
+import { type FC, useEffect } from "react";
 
 const Main: FC = () => {
 	const router = useRouter();
 	const { user } = useAuth();
 
-	if (user) {
-		router.push("/smash");
-	}
+	useEffect(() => {
+		if (user) {
+			router.replace("/smash");
+		}
+	}, [router, user]);
 
 	return (
 		<div>
-			<div className="scale-[2] my-8 ml-2 mr-8">
+			<div className="scale-[3] mb-12 ml-2 mr-8">
 				<SiteTextLogo />
 			</div>
 			<div className="flex items-center justify-center">
 				<LoginButton />
+			</div>
+			<div className="my-4">
+				<p className="px-8 text-center text-sm text-muted-foreground">
+					<Link
+						href="/terms"
+						className="underline underline-offset-4 hover:text-primary"
+					>
+						Terms of Service
+					</Link>{" "}
+					<span className="mx-2">|</span>{" "}
+					<Link
+						href="/privacy"
+						className="underline underline-offset-4 hover:text-primary"
+					>
+						Privacy Policy
+					</Link>
+				</p>
 			</div>
 		</div>
 	);
