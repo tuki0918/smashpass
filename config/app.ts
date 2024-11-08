10143116c012be48daf5bd5ed926d7a1c4e16b1b
Y.Yamamoto <tuki0918@gmail.com>
2024-11-08 13:34:54 +0900

    Add Firestore collection names and document data types for graphs and graph items

--- config/app.ts
+++ config/app.ts
@@ -3,4 +3,6 @@ export const SITE_DESCRIPTION = "";
 
 export const DB_FIRESTORE_COLLECTION_NAMES = {
 	smash: "smash-view-counters",
+	graph: "smash-graphs",
+	graph_item: "smash-graph-items",
 } as const;
--- types/firebase/firestore/models.ts
+++ types/firebase/firestore/models.ts
@@ -3,6 +3,8 @@ import type { DB_FIRESTORE_COLLECTION_NAMES } from "@/config/app";
 export type CollectionName = keyof typeof DB_FIRESTORE_COLLECTION_NAMES;
 export type CollectionDocumentDataMap = {
 	smash: SmashCounterDocumentData;
+	graph: SmashGraphDocumentData;
+	graph_item: SmashGraphItemDocumentData;
 };
 
 export type SmashCounterStatus = "published" | "draft";
@@ -11,6 +13,25 @@ export type SmashCounterDocumentData = {
 	description: string;
 	count: number;
 	status: SmashCounterStatus;
+	created_by_id: string;
+	updated_by_id: string;
+};
+
+export type SmashGraphStyle = "pie-chart" | "bar-chart";
+export type SmashGraphStatus = "published" | "draft";
+export type SmashGraphDocumentData = {
+	title: string;
+	description: string;
+	status: SmashGraphStatus;
+	style: SmashGraphStyle;
+	created_by_id: string;
+	updated_by_id: string;
+};
+
+export type SmashGraphItemDocumentData = {
+	title: string;
+	count: number;
+	graph_id: string;
 	created_by_id: string;
 	updated_by_id: string;
 };
