fbe4f799fa729b99d02ddbc5ce329e0e049b203d
Y.Yamamoto <tuki0918@gmail.com>
2024-11-16 15:21:11 +0900

    Add LanguageSelectMenu component for language selection in the page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,16 @@
+import LanguageSelectMenu from "@/components/LanguageSelectMenu";
 import Main from "@/components/Main";
 
 export default function Page() {
 	return (
 		<div className="h-screen flex items-center justify-center bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
-			<Main />
+			<div>
+				<Main />
+
+				<div className="mt-12">
+					<LanguageSelectMenu />
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ components/LanguageSelectMenu.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { routing, usePathname, useRouter } from "@/utils/i18n/routing";
+import type { Locale } from "@/utils/i18n/routing";
+import { useLocale } from "next-intl";
+import { type FC, useCallback, useMemo } from "react";
+
+const LanguageSelectMenu: FC = () => {
+	const router = useRouter();
+	const pathname = usePathname();
+	const locale = useLocale();
+	const defaultLocale = routing.locales.includes(locale as Locale)
+		? locale
+		: routing.defaultLocale;
+
+	const selectLanguage = useCallback(
+		(language: Locale) => {
+			router.replace(pathname, { locale: language });
+		},
+		[router, pathname],
+	);
+
+	return (
+		<div className="flex items-center justify-center">
+			<Select onValueChange={selectLanguage} defaultValue={defaultLocale}>
+				<SelectTrigger className="w-[180px]">
+					<SelectValue placeholder="Select a Language" />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value="ja">日本語</SelectItem>
+					<SelectItem value="en">English</SelectItem>
+				</SelectContent>
+			</Select>
+		</div>
+	);
+};
+
+export default LanguageSelectMenu;
