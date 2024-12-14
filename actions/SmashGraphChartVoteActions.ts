4b17ee15664badae07d2bb35518ceb2dcd6416f9
Y.Yamamoto <tuki0918@gmail.com>
2024-12-15 04:07:18 +0900

    fix: update type reference in SmashGraphChartVoteActions for voteItem function
    - Changed import from SmashViewCounterDocumentData to SmashGraphItemDocumentData
    - Updated type casting in the voteItem function to reflect the new data type
    
    (cherry picked from commit 6b3b7da784f1c20e36114bad4b2fa5b8f31f1166)

--- actions/SmashGraphChartVoteActions.ts
+++ actions/SmashGraphChartVoteActions.ts
@@ -2,7 +2,7 @@
 
 import type { formSchema } from "@/components/SmashGraphChartVoteForm";
 import type { DBDocument } from "@/types/firebase/firestore";
-import type { SmashViewCounterDocumentData } from "@/types/firebase/firestore/models";
+import type { SmashGraphItemDocumentData } from "@/types/firebase/firestore/models";
 import { docRef } from "@/utils/firestore";
 import { increment, setDoc } from "firebase/firestore";
 import type { z } from "zod";
@@ -14,7 +14,7 @@ export const voteItem = async (v: z.infer<typeof formSchema>) => {
 			ref,
 			{
 				count: increment(1) as unknown as number,
-			} as Partial<DBDocument<SmashViewCounterDocumentData>>,
+			} as Partial<DBDocument<SmashGraphItemDocumentData>>,
 			{ merge: true },
 		);
 	} catch (err) {
