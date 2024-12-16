a12521346ec1b4092dec787fa35f7c019d273dcb
Y.Yamamoto <tuki0918@gmail.com>
2024-12-16 19:01:14 +0900

    feat: add select-none class to SiteTextLogo component

--- components/SiteTextLogo.tsx
+++ components/SiteTextLogo.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 
 const SiteTextLogo: FC = () => {
 	return (
-		<div className="flex items-center justify-center">
+		<div className="flex items-center justify-center select-none">
 			<div className="relative">
 				<span className="font-bold text-2xl text-gray-600">123</span>
 				<div className="absolute inline -top-1.5">
