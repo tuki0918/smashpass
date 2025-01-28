4bbe0f619faccfb0a96520fac6ecbfd3ac5386d6
Y.Yamamoto <tuki0918@gmail.com>
2025-01-29 06:07:52 +0900

    🔥 Remove commented-out code for environment variable handling

--- utils/dotenv.public.ts
+++ utils/dotenv.public.ts
@@ -11,11 +11,6 @@ export const schema = zenv.object({
 });
 
 export const ENV = zenv.validate(schema, {
-	/**
-	 * Next.js will replace process.env.customKey with 'my-value' at build time. Trying to destructure process.env variables won't work due to the nature of webpack
-	 * https://nextjs.org/docs/pages/api-reference/next-config-js/env
-	 */
-	// NEXT_PUBLIC_XXX: process.env.NEXT_PUBLIC_XXX,
 	NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 	NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
 		process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
